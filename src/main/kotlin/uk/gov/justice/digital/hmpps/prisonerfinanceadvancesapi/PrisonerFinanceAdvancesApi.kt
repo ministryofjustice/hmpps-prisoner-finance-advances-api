d872e38db4f292c20a86bd88e5502da4481ed49c
runner <runner@hmpps-github-actions-runner-8f67697fd-dn6lx>
2026-09-07 15:46:01 +0000

    Rename project

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinanceadvancesapi/PrisonerFinanceAdvancesApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinanceadvancesapi/PrisonerFinanceAdvancesApi.kt
@@ -1,11 +1,11 @@
-package uk.gov.justice.digital.hmpps.templatepackagename
+package uk.gov.justice.digital.hmpps.prisonerfinanceadvancesapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class HmppsTemplateKotlin
+class PrisonerFinanceAdvancesApi
 
 fun main(args: Array<String>) {
-  runApplication<HmppsTemplateKotlin>(*args)
+  runApplication<PrisonerFinanceAdvancesApi>(*args)
 }

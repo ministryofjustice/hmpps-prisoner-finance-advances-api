0d6d89c2d6d5f7b3a71bf4afc9fa1709f39e50ac
tristan-madetech <tristan.moss@justice.gov.uk>
2026-09-16 13:56:58 +0100

    update tag

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerfinanceadvancesapi/controller/RecordController.kt
@@ -0,0 +1,66 @@
+package uk.gov.justice.digital.hmpps.prisonerfinanceadvancesapi.controller
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
+import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.prisonerfinanceadvancesapi.config.ROLE_PRISONER_FINANCE__ADVANCES__RW
+import uk.gov.justice.digital.hmpps.prisonerfinanceadvancesapi.models.request.CreateAdvanceRecordRequest
+import uk.gov.justice.digital.hmpps.prisonerfinanceadvancesapi.models.responses.AdvanceRecordResponse
+import uk.gov.justice.digital.hmpps.prisonerfinanceadvancesapi.services.RecordService
+import uk.gov.justice.hmpps.kotlin.common.ErrorResponse
+
+@Tag(name = "Advance Record Controller")
+@RestController
+class RecordController(val recordService: RecordService) {
+
+  @Operation(
+    summary = "Create a new advance record",
+    description = "Creates a new advance record for a prisoner's spends account.",
+  )
+  @ApiResponses(
+    value = [
+      ApiResponse(
+        responseCode = "201",
+        description = "Advance record Created",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = AdvanceRecordResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "400",
+        description = "Bad Request",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized - requires a valid OAuth2 token",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Forbidden - requires an appropriate role",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "500",
+        description = "Internal Server Error - An unexpected error occurred.",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
+  @SecurityRequirement(name = "bearer-jwt", scopes = [ROLE_PRISONER_FINANCE__ADVANCES__RW])
+  @PreAuthorize("hasAnyAuthority('$ROLE_PRISONER_FINANCE__ADVANCES__RW')")
+  @PostMapping("/advances")
+  fun postAdvanceRecord(@Valid @RequestBody request: CreateAdvanceRecordRequest): ResponseEntity<AdvanceRecordResponse> {
+    val createdRecordResponse = recordService.createAdvanceRecord(request)
+    return ResponseEntity.status(201).body(createdRecordResponse)
+  }
+}
